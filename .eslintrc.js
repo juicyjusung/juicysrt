0fd4aa5dd4f6026f863a6174314b858e8a15e3cf
juicyjusung <juicyjusung@gmail.com>
2020-09-28 16:17:44 +0900

    chore: Init configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    webextensions: true
+    webextensions: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'warn',
+  },
 };
